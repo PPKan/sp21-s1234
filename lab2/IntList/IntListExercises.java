790b572090cc95284b7e1b4d2025c163388d1781
PP-home-laptop <petervsjim@gmail.com>
2021-08-21 17:18:45 +0800

    modified and passed tests.

--- lab2/IntList/IntListExercises.java
+++ lab2/IntList/IntListExercises.java
@@ -14,6 +14,7 @@ public class IntListExercises {
             head.first += c;
             head = head.rest;
         }
+        head.first += c;
     }
 
     /**
@@ -33,7 +34,9 @@ public class IntListExercises {
         }
     }
 
-    /** Returns the max value in the IntList starting at L. */
+    /**
+     * Returns the max value in the IntList starting at L.
+     */
     public static int max(IntList L) {
         int max = L.first;
         IntList p = L.rest;
@@ -46,12 +49,13 @@ public class IntListExercises {
         return max;
     }
 
-    /** Returns true if the last digit of x is equal to
-     *  the first digit of x.
+    /**
+     * Returns true if the last digit of x is equal to
+     * the first digit of x.
      */
     public static boolean firstDigitEqualsLastDigit(int x) {
         int lastDigit = x % 10;
-        while (x > 10) {
+        while (x >= 10) {
             x = x / 10;
         }
         int firstDigit = x % 10;
@@ -67,16 +71,19 @@ public class IntListExercises {
      */
     public static boolean squarePrimes(IntList lst) {
         // Base Case: we have reached the end of the list
+        boolean changed = false;
+        return change(changed, lst);
+    }
+
+    private static boolean change(boolean ch, IntList lst) {
         if (lst == null) {
-            return false;
-        }
-
-        boolean currElemIsPrime = Primes.isPrime(lst.first);
-
-        if (currElemIsPrime) {
+            return ch;
+        } else if (Primes.isPrime(lst.first)) {
+            ch = true;
             lst.first *= lst.first;
+            return change(ch, lst.rest);
+        } else {
+            return change(ch, lst.rest);
         }
-
-        return currElemIsPrime || squarePrimes(lst.rest);
     }
 }
--- lab2/IntList/SquarePrimesTest.java
+++ lab2/IntList/SquarePrimesTest.java
@@ -17,4 +17,13 @@ public class SquarePrimesTest {
         assertEquals("14 -> 15 -> 16 -> 289 -> 18", lst.toString());
         assertTrue(changed);
     }
+
+    @Test
+    public void testSquarePrimesSimple1() {
+        IntList lst1 = IntList.of(3, 4, 5, 6, 7, 8, 9);
+        boolean changed1 = IntListExercises.squarePrimes(lst1);
+        assertEquals("9 -> 4 -> 25 -> 6 -> 49 -> 8 -> 9", lst1.toString());
+        assertTrue(changed1);
+    }
+
 }
