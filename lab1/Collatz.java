33235d77cf3309e8e6863f53c6827682b3f73631
Peter <petervsjim@gmail.com>
2021-09-01 21:11:24 +0800

    prefect collatz

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -2,16 +2,8 @@
  *  @author YOUR NAME HERE
  */
 public class Collatz {
-
-    /** Buggy implementation of nextNumber! */
     public static int nextNumber(int n) {
-        if (n  == 1) {
-            return 1;
-        } else if (n % 2 == 1) {
-            return 3 * n + 1;
-        } else {
-            return n / 2;
-        }
+        return n % 2 == 0 ? n/2 : 3*n + 1;
     }
 
     public static void main(String[] args) {
@@ -21,7 +13,5 @@ public class Collatz {
             n = nextNumber(n);
             System.out.print(n + " ");
         }
-        System.out.println();
     }
 }
-
