7e72ab5e308afc98fedd8884de9de0319bbf6963
Peter <petervsjim@gmail.com>
2021-09-15 22:45:29 +0800

    finished lab8 without iterator and hashset

--- lab8/hashmap/MyHashMap.java
+++ lab8/hashmap/MyHashMap.java
@@ -1,13 +1,13 @@
 package hashmap;
 
-import java.util.Collection;
+import java.util.*;
 
 /**
  *  A hash table-backed Map implementation. Provides amortized constant time
  *  access to elements via get(), remove(), and put() in the best case.
  *
  *  Assumes null keys will never be inserted, and does not resize down upon remove().
- *  @author YOUR NAME HERE
+ *  @PeterKan
  */
 public class MyHashMap<K, V> implements Map61B<K, V> {
 
@@ -26,13 +26,21 @@ public class MyHashMap<K, V> implements Map61B<K, V> {
     }
 
     /* Instance Variables */
-    private Collection<Node>[] buckets;
+//    private Collection<Node>[] buckets;
+    private Collection<Node>[] table;
+    private int size;
+    private int loadSize;
+    private double loadFactor;
     // You should probably define some more!
 
     /** Constructors */
-    public MyHashMap() { }
+    public MyHashMap() {
+        this(5);
+    }
 
-    public MyHashMap(int initialSize) { }
+    public MyHashMap(int initialSize) {
+        this(initialSize, 0.75);
+    }
 
     /**
      * MyHashMap constructor that creates a backing array of initialSize.
@@ -41,13 +49,18 @@ public class MyHashMap<K, V> implements Map61B<K, V> {
      * @param initialSize initial size of backing array
      * @param maxLoad maximum load factor
      */
-    public MyHashMap(int initialSize, double maxLoad) { }
+    public MyHashMap(int initialSize, double maxLoad) {
+        table = new Collection[initialSize];
+        loadFactor = maxLoad;
+        loadSize = initialSize;
+        size = 0;
+    }
 
     /**
      * Returns a new node to be placed in a hash table bucket
      */
     private Node createNode(K key, V value) {
-        return null;
+        return new Node(key, value);
     }
 
     /**
@@ -69,7 +82,7 @@ public class MyHashMap<K, V> implements Map61B<K, V> {
      * OWN BUCKET DATA STRUCTURES WITH THE NEW OPERATOR!
      */
     protected Collection<Node> createBucket() {
-        return null;
+        return new LinkedList<>();
     }
 
     /**
@@ -82,10 +95,147 @@ public class MyHashMap<K, V> implements Map61B<K, V> {
      * @param tableSize the size of the table to create
      */
     private Collection<Node>[] createTable(int tableSize) {
-        return null;
+        return new Collection[tableSize];
     }
 
     // TODO: Implement the methods of the Map61B Interface below
     // Your code won't compile until you do so!
+
+    /** Removes all of the mappings from this map. */
+    @Override
+    public void clear(){
+        size = 0;
+        table = new Collection[5];
+        loadSize = 5;
+        loadFactor = 0.75;
+    }
+
+    /** Returns true if this map contains a mapping for the specified key. */
+    @Override
+    public boolean containsKey(K key){
+        int keyCode = key.hashCode();
+        int position = keyCode % loadSize;
+        position = Math.abs(position);
+        if (table[position] == null) {
+            return false;
+        } else {
+            for (Node n : table[position]) {
+                if (n.key.equals(key)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+
+    /**
+     * Returns the value to which the specified key is mapped, or null if this
+     * map contains no mapping for the key.
+     */
+    @Override
+    public V get(K key){
+        int keyCode = key.hashCode();
+        int position = keyCode % loadSize;
+        position = Math.abs(position);
+        if (table[position] == null) {
+            return null;
+        } else {
+            for (Node n : table[position]) {
+                if (n.key.equals(key)) {
+                    return n.value;
+                }
+            }
+        }
+        return null;
+    }
+
+    /** Returns the number of key-value mappings in this map. */
+    @Override
+    public int size(){
+        return size;
+    }
+
+    /**
+     * Associates the specified value with the specified key in this map.
+     * If the map previously contained a mapping for the key,
+     * the old value is replaced.
+     */
+    @Override
+    public void put(K key, V value){
+
+        if (get(key) == value) {
+            return;
+        }
+
+        int keyCode = key.hashCode();
+        int position = keyCode % loadSize;
+        position = Math.abs(position);
+        Node n = createNode(key, value);
+        if (table[position] == null) {
+            table[position] = new LinkedList<>();
+        } else if (containsKey(key)) {
+            for (Node m : table[position]) {
+                if (m.key.equals(key)) {
+                    m.value = value;
+                    return;
+                }
+            }
+        }
+        table[position].add(n);
+        size += 1;
+        resize();
+
+    }
+
+    private void resize() {
+        LinkedList<Node> tempPlace = new LinkedList<>();
+        if (size / table.length > loadFactor) {
+            loadSize *= 2;
+            for (int i=0; i < table.length; i+=1) {
+                if (table[i] != null) {
+                    for (Node n : table[i]) {
+                        tempPlace.add(n);
+                    }
+                }
+            }
+            table = new Collection[loadSize];
+            size = 0;
+            for (Node n : tempPlace) {
+                put(n.key, n.value);
+            }
+        }
+    }
+
+    /** Returns a Set view of the keys contained in this map. */
+    @Override
+    public Set<K> keySet(){
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     * Removes the mapping for the specified key from this map if present.
+     * Not required for Lab 8. If you don't implement this, throw an
+     * UnsupportedOperationException.
+     */
+    @Override
+    public V remove(K key){
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     * Removes the entry for the specified key only if it is currently mapped to
+     * the specified value. Not required for Lab 8. If you don't implement this,
+     * throw an UnsupportedOperationException.
+     */
+    @Override
+    public V remove(K key, V value){
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public Iterator<K> iterator() {
+        return null;
+    }
 
 }
